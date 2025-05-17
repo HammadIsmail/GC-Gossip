723a3f6218870f2f89ab642fb0c4ccf90ad8def7
HammadIsmail <ranahammadismail@gmail.com>
2025-05-18 04:42:18 +0500

    fix

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = "mongodb+srv://itsfor017:hwSW0weqS0lV1FNt@cluster0.7pi0w.mongodb.net/universitytalks";
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
