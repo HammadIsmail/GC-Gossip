f85ab6b3f7c9be817429cc5ed3336be979e5635e
HammadIsmail <ranahammadismail@gmail.com>
2025-05-18 15:56:55 +0500

    v 0.0.1 fix

--- src/app/components/UniversityAnonymousPost.jsx
+++ src/app/components/UniversityAnonymousPost.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 export default function UniversityAnonymousPosts() {
   const departments = [
     "International Relations",
-    "Computer Science",
+
   ];
 
   const [posts, setPosts] = useState([]);
