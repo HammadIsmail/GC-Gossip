d4324828e0acabf639da96df59c8f024aa97f7fa
HammadIsmail <ranahammadismail@gmail.com>
2025-05-18 16:38:59 +0500

    v 0.0.1 fix in layout for font color 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "GCU Anonymous Posts",
@@ -24,7 +15,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-900 bg-white`}
+        className={` antialiased text-gray-900 bg-white`}
       >
         {children}
       </body>
