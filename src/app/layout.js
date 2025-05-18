3ec743183f00214d4fe47cb527c4d5d9f639b01e
HammadIsmail <ranahammadismail@gmail.com>
2025-05-18 16:08:05 +0500

    v 0.0.1 fix 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,14 +13,18 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "GCU Anonymous Posts",
-  description: "GCU Gossips is a Anonymous Posts Platform is made to give students and faculty a safe and easy way to share their thoughts, opinions, and experiences anonymously within their university community. It helps users speak freely without revealing their identity, connect with others in their department, and stay informed about what's happening across different fields of study",
+  description:
+    "GCU Gossips is an Anonymous Posts Platform made to give students and faculty a safe and easy way to share their thoughts, opinions, and experiences anonymously within their university community. It helps users speak freely without revealing their identity, connect with others in their department, and stay informed about what's happening across different fields of study",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-gray-900 bg-white`}
       >
         {children}
       </body>
